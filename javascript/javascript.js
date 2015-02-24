a1bedecf7cc5f044ed1f5505c00e8fb9256ef882
seaniechaos <seaniechaos@gmail.com>
2015-02-24 18:16:56 +0100

    Added the first line of the amortization table.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,13 +6,13 @@ var MortgageCalculator = function () {
   this.years;
   this.monthlyPayment;
   this.currentOpeningBalance;
+  this.month = 1;
   this.addListener();
 
 };
 
 MortgageCalculator.prototype.getInputData = function (event) {
   event.preventDefault();
-  debugger;
   this.askPrice             = document.getElementById("ask_price").value;
   this.downPayment          = document.getElementById("dp_submitted").value;
   this.interestRateAnnual   = document.getElementById("interest_rate").value
@@ -22,48 +22,43 @@ MortgageCalculator.prototype.getInputData = function (event) {
 };
 
 MortgageCalculator.prototype.addListener = function () {
-  debugger;
   document.getElementById("submit").addEventListener("click", this.getInputData.bind(this))
 };
 
 MortgageCalculator.prototype.calculation = function () {
-  monthlyPayment = finance.calculatePayment(this.principal, this.years * 12, this.interestRateAnnual);
-  this.render();
+  this.monthlyPayment = finance.calculatePayment(this.principal, this.years * 12, this.interestRateAnnual);
+  this.openingBalance = Math.round(this.principal * 100) / 100;
+  this.interestComponent = Math.round(this.openingBalance * this.interestRateAnnual / 12 / 100 * 100) / 100;
+  this.principalReduction = this.monthlyPayment - this.interestComponent;
+  this.closingBalance = this.openingBalance - this.principalReduction;
+  this.renderPayment();
+  this.renderRow();
 };
 
-MortgageCalculator.prototype.render = function () {
-  paymentElement = $("<p>").html(monthlyPayment);
+MortgageCalculator.prototype.renderPayment = function () {
+  paymentElement = $("<p>").html(this.monthlyPayment);
   $("#payments").append(paymentElement);
-}
+};
+
+MortgageCalculator.prototype.iterateUntilFinished = function () {
+  // update openingBalance
+  // update interestComponent
+  // principalReduction = monthlyPayment - interestComponent
+  // update closingBalance
+  // update this.month
+  // call renderRow
+};
+
+MortgageCalculator.prototype.renderRow = function () {
+  var $row                = $("<tr>");
+  var $monthData          = $("<td>").html(this.month);
+  var $openingData        = $("<td>").html(this.openingBalance);
+  var $paymentData        = $("<td>").html(this.monthlyPayment)
+  var $interestPart       = $("<td>").html(this.interestComponent);
+  var $principalRed       = $("<td>").html(this.principalReduction);
+  var $closingBal         = $("<td>").html(this.closingBalance);
+  $row.append($monthData).append($openingData).append($paymentData).append($interestPart).append($principalRed).append($closingBal);
+  $("#payments-table").append($row);
+};
 
 var calculator = new MortgageCalculator();
-
-// document.getElementById("submit").addEventListener("click", function (event) {
-//   event.preventDefault();
-
-//   var askPrice        = document.getElementById("ask_price").value;
-//   var downPayment     = document.getElementById("dp_submitted").value;
-//   var principal       = askPrice - downPayment;
-
-//   var interestRateAnnual   = document.getElementById("interest_rate").value
-//   var interestRateMonthly  = interestRateAnnual / 12;
-//   var years                = document.getElementById("years").value;
-//   var timeToMaturity       = years * 12;
-
-
-//   var monthlyPayment = finance.calculatePayment(principal, timeToMaturity, interestRateAnnual);
-  
-  
-//   var openingBalance  = principal;
-//   var interestPortion = (openingBalance * interestRateMonthly/100);
-//   var principalReduct = monthlyPayment - interestPortion;
-//   var closingBalance  = openingBalance - principalReduct;
-
-//   var paymentElement = $("<p>").html(monthlyPayment);
-//   $("#payments").append(paymentElement);
-//   debugger;
-
-
-// });
-
-
